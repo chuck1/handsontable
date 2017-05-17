f08db53936d61d377450399fb6de228dab54cee7
Charles Rymal <charles.rymal@nortek.com>
2017-05-17 08:56:56 -0700

    docs. renaming to fix root setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../sheets'))
+sys.path.insert(0, os.path.abspath('../../storage_pkg'))
+sys.path.insert(0, os.path.abspath('../../sheets_pkg'))
+sys.path.insert(0, os.path.abspath('../../sheets_backend_pkg'))
 
 # -- General configuration ------------------------------------------------
 
